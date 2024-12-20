d40996f90bf7e2ea82e0654e7dc90d9f56279dc6
henrygagnier <hg585@proton.me>
2024-12-19 21:58:48 -0500

    fix

--- auth/routes/auth.js
+++ auth/routes/auth.js
@@ -71,7 +71,7 @@ router.get("/auth/user", async (req, res) => {
 
     res.status(200).json(user);
   } catch (err) {
-    res.status(500).json({ message: "Failed to fetch user data" });
+    res.status(500).json({ message: err });
   }
 });
 
