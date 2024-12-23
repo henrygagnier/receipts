7a2c0382dca4826cd0499e4844f82cea715c7179
henrygagnier <hg585@proton.me>
2024-12-23 10:28:14 -0500

    fix: revert

--- api/receipt_parser.py
+++ api/receipt_parser.py
@@ -9,7 +9,6 @@ from difflib import get_close_matches
 import dateutil.parser
 from dateutil.parser import parse
 import imutils
-from imutils.perspective import four_point_transform
 
 # Load the config from a JSON file
 def load_config(config_path):
@@ -32,49 +31,12 @@ def process_receipt_image(image_path: str) -> Dict[str, Any]:
     image = imutils.resize(image, width=500)
     ratio = img_orig.shape[1] / float(image.shape[1])
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(
-        gray,
-        (
-            5,
-            5,
-        ),
-        0,
-    )
-    edged = cv2.Canny(blurred, 75, 200)
-    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-
-    # initialize a contour that corresponds to the receipt outline
-    receiptCnt = None
-    # loop over the contours
-    for c in cnts:
-        # approximate the contour
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-        # if our approximated contour has four points, then we can
-        # assume we have found the outline of the receipt
-        if len(approx) == 4:
-            receiptCnt = approx
-            break
-    if receiptCnt is None:
-        raise Exception(
-            (
-                "Could not find receipt outline. "
-            "Try debugging your edge detection and contour steps."
-        )
-        )
-
-    receipt = four_point_transform(img_orig, receiptCnt.reshape(4, 2) * ratio)
-    options = "--psm 6"
-    text = pytesseract.image_to_string(
-        cv2.cvtColor(receipt, cv2.COLOR_BGR2RGB), config=options
-    )
+    
     text2 = pytesseract.image_to_string(
         cv2.cvtColor(gray, cv2.COLOR_BGR2RGB), config=options
     )
 
-    return(parse_receipt(conf, text), parse_receipt(conf, text2))
+    return(parse_receipt(conf, text2))
 
 def normalize(lines):
     """
