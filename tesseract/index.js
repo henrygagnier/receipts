99d220fb7f88307c3d08890767726f24439818d8
henrygagnier <hg585@proton.me>
2024-12-23 22:16:06 -0500

    fix: tests

--- tesseract/index.js
+++ tesseract/index.js
@@ -6,7 +6,7 @@ const port = 3001;
 
 app.post("/", async (req, res) => {
   const worker = await createWorker("eng");
-  const ret = await worker.recognize("./images/image.jpg");
+  const ret = await worker.recognize("images/image.jpg");
   console.log(ret.data.text);
   res.send(ret.data.text);
 });
